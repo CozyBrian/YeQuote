7e3fe70d1fc1cd82b59c9edc64b3150c547b02f8
CozyBrian <dragonbrian33@gmail.com>
2022-02-16 03:42:40 +0000

    feat: Drawer navigation

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { YeApp } from './src/components/kanyescreen';
+import { AppNavigation } from './src/navigation/app-navigation';
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <YeApp />
+      <AppNavigation />
     </>
   );
 }
---
+++ src/navigation/app-navigation.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Button, View } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { MenuIcon } from "../utils/icons";
+import { YeApp } from "../components/kanyescreen";
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerShown: false,
+          swipeEnabled: false,
+          drawerStyle: { width: 220 },
+        }}
+        drawerContent={(props) => {
+          return (
+            <>
+              <View style={{ paddingTop: 70 }}>
+                <DrawerItemList {...props} />
+              </View>
+              <MenuIcon
+                color="black"
+                onPress={() => props.navigation.toggleDrawer()}
+              />
+            </>
+          );
+        }}
+      >
+        <Drawer.Screen name="Home" component={YeApp} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
